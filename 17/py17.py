c1a1238166ee02bd4ddcb29ca9ba39316bc9c06f
WeiXiao <1197749258@qq.com>
2017-12-26 11:57:55 +0800

    add comment

--- 17/py17.py
+++ 17/py17.py
@@ -1,17 +1,17 @@
-from xml.dom.minidom import Document
-import xlrd
+from xml.dom.minidom import Document #写xml的库
+import xlrd #读excel库
 
 def xsl_to_xml(f_name, inf):
     doc = Document()
-    root = doc.createElement('root')
-    doc.appendChild(root)
+    root = doc.createElement('root')#根
+    doc.appendChild(root)#添加节点
     student = doc.createElement('student')
     root.appendChild(student)
     com = doc.createComment('学生信息表\n\t\t"id" : [名字, 数学, 语文, 英文]')
     student.appendChild(com)
     student.appendChild(doc.createTextNode('{'))
     for e in inf:
-        a = doc.createTextNode('"%s" : ["%s", %s, %s, %s],'%tuple(e))
+        a = doc.createTextNode('"%s" : ["%s", %s, %s, %s],'%tuple(e))#列表转元组
         student.appendChild(a)
     student.appendChild(doc.createTextNode('}'))
     with open(f_name+'.xml', 'w') as f:
@@ -22,8 +22,8 @@ def get_inf(f_name):
     inf = []
     with xlrd.open_workbook(f_name+'.xls') as f:
         table = f.sheets()[0]
-        for i in range(table.nrows):
-            inf.append(table.row_values(i))
+        for i in range(table.nrows):#获取行数
+            inf.append(table.row_values(i))#获取每一行内容
     return inf
 
 if __name__ == '__main__':
